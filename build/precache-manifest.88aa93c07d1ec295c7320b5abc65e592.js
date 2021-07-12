21972f7358403bb2b532f8a05ea517b7f3eb640a
Rajita Roy <royrajita6@gmail.com>
2021-07-12 13:30:42 +0530

    Deployed

---
+++ build/precache-manifest.88aa93c07d1ec295c7320b5abc65e592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e4ac70ffd0d32a6f1511557d2a1d374",
+    "url": "/Quiz-App/index.html"
+  },
+  {
+    "revision": "8258ec8e06ab964c122b",
+    "url": "/Quiz-App/static/css/main.dc2b3d24.chunk.css"
+  },
+  {
+    "revision": "34c3fafb4817ce8be14a",
+    "url": "/Quiz-App/static/js/2.71dc2452.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Quiz-App/static/js/2.71dc2452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8258ec8e06ab964c122b",
+    "url": "/Quiz-App/static/js/main.149ea72f.chunk.js"
+  },
+  {
+    "revision": "8ff3944e0bd723db61e9",
+    "url": "/Quiz-App/static/js/runtime-main.6a5e80b8.js"
+  }
+]);
